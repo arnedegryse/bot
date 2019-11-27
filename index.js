4d26e7f571bd308e1031a05b2a6e54407b6abbe7
arnedegryse <58048182+arnedegryse@users.noreply.github.com>
2019-11-27 13:46:28 +0100

    Update index.js

--- index.js
+++ index.js
@@ -202,26 +202,26 @@ bot.on("message", async message => {
 
     if (command === `${prefix}clear`) {
 
-
+        if (!message.member.hasPermissions("MANAGE_MESSAGES")) return message.reply("Je hebt geen toestemming");
 
         if (!arguments[0]) return message.channel.send("Geef een aantal op.");
 
-        if (Number.isInteger(parseInt(args[0]))) {
+        if (Number.isInteger(parseInt(arguments[0]))) {
 
-            var amount = parseInt(args[0]) + 1;
+            var amount = parseInt(arguments[0]) + 1;
 
             message.channel.bulkDelete(amount).then(() => {
-                if (args[0] == 0) {
+                if (arguments[0] == 0) {
 
                     message.channel.send(`Ben je incapabel ofzo? Ik kan geen 0 berichten verwijderen`).then(msg => msg.delete(3000));
 
-                } else if (args[0] == 1) {
+                } else if (arguments[0] == 1) {
 
                     message.channel.send(`Ik heb 1 bericht verwijderd.`).then(msg => msg.delete(3000));
 
                 } else {
 
-                    message.channel.send(`Ik heb ${args[0]} berichten verwijderd`).then(msg => msg.delete(3000));
+                    message.channel.send(`Ik heb ${arguments[0]} berichten verwijderd`).then(msg => msg.delete(3000));
                 }
 
 
