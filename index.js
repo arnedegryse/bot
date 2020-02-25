39f7b52cb26b72db520ac1f28124394a7665b1d3
arnedegryse <58048182+arnedegryse@users.noreply.github.com>
2020-02-25 18:29:00 +0100

    Update index.js

--- index.js
+++ index.js
@@ -230,7 +230,7 @@ bot.on("message", async message => {
             .addField("Suggestie", suggestie)
             .addField("Ingezonden door", message.author);
 
-        var suggestieChannel = message.guild.channels.find(x => x.name === "❗suggestie");
+        var suggestieChannel = message.guild.channels.find(x => x.name === "suggestie");
         if (!suggestieChannel) return message.channel.send("Kanaal niet gevonden");
 
         suggestieChannel.send(suggestieEmbed).then(embedMessage => {
