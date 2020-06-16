71f2e5960c848104b9425837eaec217a07986b3e
arnedegryse <58048182+arnedegryse@users.noreply.github.com>
2020-06-16 22:35:08 +0200

    Update index.js

--- index.js
+++ index.js
@@ -197,38 +197,38 @@ bot.on("message", async message => {
 
     //clear command
 
-    if (command === `${prefix}clear`) {
+    // if (command === `${prefix}clear`) {
 
-        if (!message.member.hasPermissions("MANAGE_MESSAGES")) return message.reply("Je hebt geen toestemming");
+//if (!message.member.hasPermissions("MANAGE_MESSAGES")) return message.reply("Je hebt geen toestemming");
 
-        if (!arguments[0]) return message.channel.send("Geef een aantal op.");
+    //    if (!arguments[0]) return message.channel.send("Geef een aantal op.");
 
-        if (Number.isInteger(parseInt(arguments[0]))) {
+    //    if (Number.isInteger(parseInt(arguments[0]))) {
 
-            var amount = parseInt(arguments[0]) + 1;
+       //     var amount = parseInt(arguments[0]) + 1;
 
-            message.channel.bulkDelete(amount).then(() => {
-                if (arguments[0] == 0) {
+      //      message.channel.bulkDelete(amount).then(() => {
+      //          if (arguments[0] == 0) {
 
-                    message.channel.send(`Ben je incapabel ofzo? Ik kan geen 0 berichten verwijderen`).then(msg => msg.delete(3000));
+         //           message.channel.send(`Ben je incapabel ofzo? Ik kan geen 0 berichten verwijderen`).then(msg => msg.delete(3000));
 
-                } else if (arguments[0] == 1) {
+          //      } else if (arguments[0] == 1) {
 
-                    message.channel.send(`Ik heb 1 bericht verwijderd.`).then(msg => msg.delete(3000));
+              //      message.channel.send(`Ik heb 1 bericht verwijderd.`).then(msg => msg.delete(3000));
 
-                } else {
+           //     } else {
 
-                    message.channel.send(`Ik heb ${arguments[0]} berichten verwijderd`).then(msg => msg.delete(3000));
-                }
+// message.channel.send(`Ik heb ${arguments[0]} berichten verwijderd`).then(msg => msg.delete(3000));
+               // }
 
 
-            });
+         //   });
+//
+       // } else {
+   //         return message.channel.send("Geef een aantal op.");
+       // }
 
-        } else {
-            return message.channel.send("Geef een aantal op.");
-        }
-
-    }
+  //  }
 
 
     //Suggestie command
